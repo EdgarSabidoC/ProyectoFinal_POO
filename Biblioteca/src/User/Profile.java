fa0e34c3a4828f2cfe2f04c7f50d633aa8f5542d
EdgarSabidoC <a12004194@alumnos.uady.mx>
2021-06-02 17:47:05 -0500

    Se agregaron nuevas clases y se implemento una interfaz profile

---
+++ Biblioteca/src/User/Profile.java
@@ -0,0 +1,13 @@
+package User;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author edgar
+ */
+public interface Profile {
+    void rename(String name, String firstLastName, String secondLastName); // Cambia el nombre y apellidos.
+    boolean authenticate(char[] password); // Verifica que el perfil sea el correcto.
+    int identity(); // Retorna la identidad del usuario.
+}
