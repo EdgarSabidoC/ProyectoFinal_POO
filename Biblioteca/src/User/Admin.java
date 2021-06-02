39f4502a832c53b3639730e6c8ef9d7a62da42bc
EdgarSabidoC <a12004194@alumnos.uady.mx>
2021-06-02 17:47:05 -0500

    Se agregaron nuevas clases y se implemento una interfaz profile

--- Biblioteca/src/User/Admin.java
+++ Biblioteca/src/User/Admin.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package User;
+
+import java.util.UUID;
 
 /**
  *
  * @author edgar
  */
-public class Admin {
+public class Admin extends User {
+    private UUID adminID;
+    private Password adminPassword;
+    
+    public Admin(String name, String firstLastName, String secondLastName) {
+        super(name, firstLastName, secondLastName);
+        setAdminID(UUID.randomUUID());
+    }
+    
+    
+    
+    // Getters:
+    private UUID getAdminID() {
+        return adminID;
+    }
+    
+    // Setters:
+    private void setAdminID(UUID adminID) {
+        this.adminID = adminID;
+    }
+    
+    
+    // Métodos concretos:
+    @Override
+    public void rename(String name, String firstLastName, String secondLastName) {
+        super.setName(name);
+        super.setFirstLastName(firstLastName);
+        super.setSecondLastName(secondLastName);
+    }
+    
+    // Verifica que el password del Admin sea correcto:
+    @Override
+    public boolean authenticate(char[] password) {
+        // Aquí va la validación.
+        if(getPassword.equals(password))
+    }
+
+    // Retorna el número de indentificación del admin:
+    @Override
+    public int identity() {
+        return getAdminID().hashCode();
+    }
+
+    // Método toString:
+    @Override
+    public String toString() {
+        return super.toString() + ", adminID: " + getAdminID();
+    }
+    
+    
     
 }
